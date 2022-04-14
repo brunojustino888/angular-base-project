8195e1e294c48786b3bf8c0b276d34565ea8afd7
bruno.justino <bruno.justino@gps-pamcary.com.br>
2022-04-14 06:45:47 -0300

    Adicionada as configuracoes bootstrap, jquery e popper.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; 
 
 import { AppComponent } from './app.component';
 
 import { Aula02Module } from './aula02/aula02.module';
-import { Aula03Component } from './aula03/aula03.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    Aula02Module,
+    BrowserModule, 
+    Aula02Module
   ],
   providers: [],
   bootstrap: [AppComponent]
