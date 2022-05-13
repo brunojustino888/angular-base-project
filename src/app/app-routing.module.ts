352931e644f5cafa6b01085538b43259163d1b8d
bruno.justino <bruno.justino@gps-pamcary.com.br>
2022-05-13 03:58:06 -0300

    Prototipo do cadastro de pessoas adicionado, ajustes no menu e alinhamento do form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { Aula00Component } from "./aula00/aula00.component";
 import { Aula01Component } from "./aula01/aula01.component"; 
 import { Aula02Component } from "./aula02/aula02.component";
+import { Aula03Component } from "./aula03/aula03.component";
 
 const APP_ROUTES: Routes= [
     {path:'' , component: Aula00Component},
     {path:'aula00' , component: Aula00Component}, 
     {path:'aula01' , component: Aula01Component}, 
-    {path:'aula02' , component: Aula02Component}, 
+    {path:'aula02' , component: Aula02Component},
+    {path:'aula03' , component: Aula03Component},
 ]
 
 @NgModule({
@@ -17,4 +19,4 @@ const APP_ROUTES: Routes= [
     exports:[RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule {} 
---
+++ src/app/aula03/aula03.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aula03',
+  templateUrl: './aula03.component.html',
+  styleUrls: ['./aula03.component.css']
+})
+export class Aula03Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
